fb08dd149fede0745d6705db1bad7d6c7a7da0d4
TheDragonary <ibrahimzaman2004@gmail.com>
2023-04-21 21:03:59 +0100

    Update

--- install.sh
+++ install.sh
@@ -2,32 +2,35 @@
 echo "Installing yay"
 sudo pacman -S --noconfirm --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 
-echo "Installing apps and packages"
-sudo pacman -S --noconfirm firefox neofetch git broadcom-wl plymouth kio-gdrive unzip zip discord steam kitty zsh vi htop vim qbittorrent flameshot xdg-desktop-portal
+echo "Installing apps and packages..."
+sudo pacman -S --noconfirm firefox neofetch git broadcom-wl plymouth kio-gdrive unzip zip discord steam kitty zsh vi htop vim qbittorrent flameshot xdg-desktop-portal ffmpeg ffmpegthumbs mpv
 
-echo "Installing fonts"
+echo "Installing fonts..."
 sudo pacman -S --noconfirm noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ttf-jetbrains-mono ttf-fira-code ttf-liberation ttf-roboto ttf-ubuntu-font-family && yay -S --noconfirm ttf-meslo-nerd-font-powerlevel10k
 
-echo "Installing Spotify"
+echo "Installing Spotify..."
 yay -S --noconfirm spotify
 bash <(curl -sSL https://raw.githubusercontent.com/SpotX-CLI/SpotX-Linux/main/install.sh) -ce
 
-echo "Installing AUR packages"
+echo "Installing AUR packages..."
 yay -S --noconfirm cava cmatrix cbonsai neo-matrix-git ani-cli
 
-echo "Changing shell to zsh"
+echo "Installing GStreamer codecs..."
+sudo pacman -S --noconfirm gstreamer gst-libav gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly && yay -S --noconfirm gst-plugin-libde265 gst-plugins-openh264
+
+echo "Changing shell to zsh..."
 sudo chsh -s /bin/zsh $USER
 echo $SHELL
 
-echo "Installing zsh4humans"
+echo "Installing zsh4humans..."
 if command -v curl >/dev/null 2>&1; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/romkatv/zsh4humans/v5/install)"
 else
   sh -c "$(wget -O- https://raw.githubusercontent.com/romkatv/zsh4humans/v5/install)"
 fi
 
-echo "Cloning dotfiles bare repo"
+echo "Cloning dotfiles bare repo..."
 git clone --bare https://github.com/TheDragonary/dotfiles.git $HOME/.dotfiles && alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
-echo "Pushing dotfiles"
+echo "Pushing dotfiles..."
 dotfiles checkout -f
